d9f5fac22e0ec8320584f85538b0ccbf73d8c074
Prabhat Kumar <prabhat.kumar.hisar@gmail.com>
2024-10-26 21:27:34 +0530

    Remove animtion for the message text in app.css

--- src/App.js
+++ src/App.js
@@ -4,24 +4,16 @@ import Terminal from './Components/Terminal';
 import name from './assets/Images/output-onlinetools-removebg-preview.png'
 
 function App() {
-  document.addEventListener("DOMContentLoaded", function () {
-    const firstText = document.querySelector(".first-text");
-    const secondText = document.querySelector(".second-text");
-    firstText.addEventListener("animationend", function () {
-      secondText.style.display = "block";
-      secondText.classList.add("animate");
-    });
-  });
   return (
     <div >
       <header>
         <img src={name} alt="name" />
       </header>
       <div className='text-container'>
-        <h3 className='message-text first-text'>
+        <h3 className='message-text'>
           Pleased to have you here, fellow homo sapiens.
         </h3>
-        <h3 className='message-text second-text animate'>
+        <h3 className='message-text'>
           Type
           <span className='help-text'>'help'</span>
           to view a list of available
